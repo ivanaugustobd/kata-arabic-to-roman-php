a8c5cdee4b38814a51ad8e8bd4b8d37994df061e
Ivan Augusto <ivanaugustobd@gmail.com>
2018-03-25 19:53:34 -0300

    Basic archtecture created

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
+use ArabicToRoman\Converter;
+
 require __DIR__ . '/vendor/autoload.php';
+
+if (!isset($argv[1])) {
+    echo 'Usage: php index.php <arabic_number>' . PHP_EOL;
+    exit(1);
+}
+
+$arabicNumber = $argv[1];
+
+echo convert($arabicNumber);
+
+exit(0);
---
+++ src/converter.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Convert given arabic number into roman equivalent
+ *
+ * @param int $arabicNumber Arabic number to be converted
+ * @return string Roman algorism
+ */
+function convert(int $arabicNumber)
+{
+    // 
+}
