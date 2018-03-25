b438da5f275d55f108fde0d75d98561d99ca4f5b
Ivan Augusto <ivanaugustobd@gmail.com>
2018-03-25 20:20:57 -0300

    Add conversion of algorisms with one letter only

--- src/Converter.php
+++ src/Converter.php
@@ -4,16 +4,29 @@ namespace ArabicToRoman;
 
 class Converter
 {
+    const ALGORISMS_MAP = [
+        1 => 'I',
+        5 => 'V',
+        10 => 'X',
+        50 => 'L',
+        100 => 'C',
+        1000 => 'M',
+    ];
+
     /**
      * Convert given arabic number into roman equivalent
      *
-     * @param int $arabicNumber Arabic number to be converted
+     * @param int $arabic Arabic number to be converted
      * @return string Roman algorism
      */
-    public static function convert(int $arabicNumber) : string
+    public static function convert(int $arabic) : string
     {
-        if ($arabicNumber === 0) {
+        if ($arabic === 0) {
             return '';
+        }
+
+        if (isset(self::ALGORISMS_MAP[$arabic])) {
+            return self::ALGORISMS_MAP[$arabic];
         }
     }
 }
--- tests/converterTest.php
+++ tests/converterTest.php
@@ -9,4 +9,24 @@ class ConverterTest extends TestCase
     {
         $this->assertEquals('', Converter::convert(0));
     }
+
+    /**
+     * @dataProvider oneAlgorismNumbers
+     */
+    public function testOneAlgorismNumber($arabic, $roman)
+    {
+        $this->assertEquals($roman, Converter::convert($arabic));
+    }
+
+    public function oneAlgorismNumbers()
+    {
+        return [
+            [1, 'I'],
+            [5, 'V'],
+            [10, 'X'],
+            [50, 'L'],
+            [100, 'C'],
+            [1000, 'M'],
+        ];
+    }
 }
